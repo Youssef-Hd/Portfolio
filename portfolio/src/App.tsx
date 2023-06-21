42ccdb20e8d9d0e86804bb63c9f9a32c8b7afe39
youssef-hd <youssefhamadi94@gmail.com>
2023-06-21 11:45:39 +0300

    card closed still needs hover and padding

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -3,15 +3,17 @@ import { motion, spring } from "framer-motion";
 import "./App.css";
 import usef from "../src/assets/Usef.png";
 import Ellipse from "../src/assets/Ellipse 5.png";
+import { SiMinutemailer } from "react-icons/si";
+import { FcIphone } from "react-icons/fc";
 
 function App() {
   const [toggle, setToggle] = useState(false);
 
   return (
     <>
-      <div className="app">
+      <div className={toggle ? "appopen" : "app"}>
         <motion.div
-          transition={{ layout: { duration: 1, type: "spring" } }}
+          transition={{ layout: { duration: 1, type: spring } }}
           layout
           onClick={() => setToggle(!toggle)}
           className="card"
@@ -20,24 +22,53 @@ function App() {
             boxShadow: "0px 10px 30px rgba(0,0,0, 0.5)",
           }}
         >
-          <motion.h2 layout="position" className="div__card--title-h2">
+          <motion.h1
+            transition={{ layout: { duration: 1, type: spring } }}
+            layout="position"
+            className="div__card--title-h1"
+          >
             Full Stack Web Developer
-          </motion.h2>
-          <motion.div className="div__card--closed">
+          </motion.h1>
+          <motion.div className={toggle ? "none" : "div__card--closed"}>
             <div>
-              <h3 className="div__card--closed-h3">Youssef Hamadi</h3>
-              <img src={usef} className="photo__img" />
+              <motion.h3 layout="position" className="div__card--closed-h3">
+                Youssef Hamadi
+              </motion.h3>
+              <motion.div className="img-divv">
+                <img src={usef} className="photo__img" />
+              </motion.div>
             </div>
-            <div className="div__card--closed-h4-p">
-              <h4 className="div__card--closed-h4">About Me</h4>
-              <p className="div__card--closed-p">
+            <div className="div__card--closed-h4-p-img">
+              <motion.h3
+                layout="position"
+                className="div__card--closed-h3-about"
+              >
+                About Me
+              </motion.h3>
+              <motion.p layout="position" className="div__card--closed-p">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut,
                 dolore.Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                 Ut, dolore.Lorem ipsum dolor, sit amet consectetur adipisicing
                 elit. Ut, dolore.
-              </p>
-              <div>
-                <img src={Ellipse} className="div__card--closed-ellipse" />
+              </motion.p>
+
+              <div className="div__card--closed-ellipse">
+                <img className="ww" src={Ellipse} />
+                <div className="label__phone">
+                  <div className="label__phone-div">
+                    <FcIphone size={32} />
+                    +961 76645651
+                  </div>
+                  <div className="div__card--closed-email">
+                    <SiMinutemailer
+                      size={32}
+                      color="rgb(227, 137, 57)"
+                      style={{ marginRight: "10px" }}
+                      className="ss"
+                    />
+                    Youssefhamadi94@gmail.com
+                  </div>
+                </div>
               </div>
             </div>
           </motion.div>
@@ -45,9 +76,11 @@ function App() {
             <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
-              transition={{ duration: 1 }}
+              transition={{ duration: 0.5 }}
               exit={{ opacity: 0 }}
+              className="div__card-open"
             >
+              <img  className='card__open-img' src={usef} />
               <p>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui
                 commodi quo placeat rerum veniam. Vitae est iure commodi cum
